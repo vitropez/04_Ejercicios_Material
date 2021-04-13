f912fee6f158eceeef47fac34418a5c45328df8d
vitropez <64587299+vitropez@users.noreply.github.com>
2021-04-13 18:20:35 +0200

    13 Abril resueltos

--- js/Ejercicio_4-5.js
+++ js/Ejercicio_4-5.js
@@ -2,7 +2,13 @@
 
 
 function obtenerLetra(){
+    let letras ="TRWAGMYFPDXBNJZSQVHLCKET";
 
-		//  TRWAGMYFPDXBNJZSQVHLCKET  
+   let tarjeta=document.getElementById("card")
+   DNI=tarjeta.value
+console.log(DNI)
+    posicion = DNI % 23;
+    letra = letras.charAt(posicion);
+    tarjeta.value=( DNI+"-"+letra);
 
 }
